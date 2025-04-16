348c4bededc4d3500515d9943265d6cb8e13defb
Oxtiz <oxtizdan_job@gmail.com>
2025-04-16 15:25:12 +0300

    Fix bugs RAG_payment_bot

--- projects/RAG_payment_bot/rag/__init__.py
+++ projects/RAG_payment_bot/rag/__init__.py
@@ -1,10 +1,11 @@
+from os import getenv
 from llama_index.llms.openrouter import OpenRouter
 from llama_index.core import Settings
 from .logic import BM25, chat_query_engine
 import asyncio
 
 Settings.llm = OpenRouter(
-    api_key="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",
+    api_key=getenv("OPEN_ROUTER_TOKEN"),
     max_tokens=256,
     context_window=4096,
     model="meta-llama/llama-3.1-8b-instruct:free",
